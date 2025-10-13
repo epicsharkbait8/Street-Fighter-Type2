a8e0267c8cfa167325c00a1bb6c632bc56328e86
Thomas Garcia <144739053+epicsharkbait8@users.noreply.github.com>
2025-10-13 18:23:28 +0000

    added 2nd player, new models

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+// A starter template for side-scrolling games like our platformer
 kaboom({
  width: 1200,
  height: 600,
@@ -7,12 +8,22 @@ kaboom({
 setGravity(800);
 
 // Load a player sprite
-loadSprite("apple", "https://kaboomjs.com/sprites/apple.png");
+loadSprite("foo", "New Piskel-1.png.png");
+loadSprite("foo2", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQccF1S-NPYFMJ1Hey62chXLeoGyl13zJYqiQ&s");
+//hopefully the world
+
 
 // --- The Player Character ---
 const player = add([
- sprite("apple"),
+ sprite("foo"),
  pos(100, 100),
+ area({ scale: 5 }),
+ body(),
+]);
+
+const player2 = add([
+ sprite("foo2"),
+ pos(1000, 100),
  area({ scale: 0.7 }),
  body(),
 ]);
@@ -32,8 +43,22 @@ onKeyDown("left", () => {
 onKeyDown("right", () => {
  player.move(200, 0);
 });
-onKeyPress("space", () => {
+onKeyPress("up", () => {
  if (player.isGrounded()) {
  player.jump(650);
  }
 });
+
+onKeyDown("a", () => {
+ player2.move(-200, 0);
+});
+onKeyDown("d", () => {
+ player2.move(200, 0);
+});
+onKeyPress("w", () => {
+ if (player.isGrounded()) {
+ player2.jump(650);
+ }
+});
+
+go("main")
